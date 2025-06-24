12278b77da54e4de05af6eded281264d3207e20d
Kirill Filippov <vip.kirya.filippov.2002@gmail.com>
2025-06-24 15:39:53 +0400

    cart fix

--- server/controllers/cartContollers.js
+++ server/controllers/cartContollers.js
@@ -14,7 +14,7 @@ exports.getCartByUserId = async (req, res) => {
         p.description,
         p.price,
         p.characteristics
-      FROM cart_items c
+      FROM cart c
       JOIN products p ON c.product_id = p.id
       WHERE c.user_id = $1
     `, [userId]);
@@ -32,7 +32,7 @@ exports.addToCart = async (req, res) => {
 
   try {
     const result = await pool.query(
-      'INSERT INTO cart_items (user_id, product_id, quantity) VALUES ($1, $2, $3) ON CONFLICT (user_id, product_id) DO UPDATE SET quantity = cart_items.quantity + $3 RETURNING *',
+      'INSERT INTO cart (user_id, product_id, quantity) VALUES ($1, $2, $3) ON CONFLICT (user_id, product_id) DO UPDATE SET quantity = cart.quantity + $3 RETURNING *',
       [userId, productId, quantity]
     );
     res.status(201).json(result.rows[0]);
@@ -49,11 +49,11 @@ exports.updateCartItem = async (req, res) => {
   try {
     if (quantity <= 0) {
       // Если количество 0 или меньше, удаляем товар из корзины
-      await pool.query('DELETE FROM cart_items WHERE user_id = $1 AND product_id = $2', [userId, productId]);
+      await pool.query('DELETE FROM cart WHERE user_id = $1 AND product_id = $2', [userId, productId]);
       res.status(204).send();
     } else {
       const result = await pool.query(
-        'UPDATE cart_items SET quantity = $1 WHERE user_id = $2 AND product_id = $3 RETURNING *',
+        'UPDATE cart SET quantity = $1 WHERE user_id = $2 AND product_id = $3 RETURNING *',
         [quantity, userId, productId]
       );
       res.json(result.rows[0]);
@@ -68,7 +68,7 @@ exports.deleteFromCart = async (req, res) => {
   const { userId, productId } = req.params;
 
   try {
-    await pool.query('DELETE FROM cart_items WHERE user_id = $1 AND product_id = $2', [userId, productId]);
+    await pool.query('DELETE FROM cart WHERE user_id = $1 AND product_id = $2', [userId, productId]);
     res.status(204).send();
   } catch (err) {
     console.error(err);
